d50112bf8904141dbdbb4864257feb5db4702f5d
joaolotjr <joaolotjr@gmail.com>
2022-06-29 15:29:09 -0300

    studying Async/await

--- Previous Study/studies.js
+++ Previous Study/studies.js
@@ -1,20 +1,35 @@
 
-// -> Promise
+// // -> Promise
+// const myPromise = new Promise((resolve, reject) => {
+//     window.setTimeout(() => {
+//         resolve('Resolved');
+//     }, 2000);
+// });
+
+// await myPromise
+//     .then((result) => result + 'passing by then')
+//     .then((result) => result + 'and now it is over!!')
+//     .catch((err) => console.log(err.message));
+
+
+//     // After 2 seconds, it will return the value:
+//     // "Resolved passing by then and now it is over!!"
+
+// // -> Async/await
+
+async function resolvePromise(){
 const myPromise = new Promise((resolve, reject) => {
     window.setTimeout(() => {
         resolve('Resolved');
-    }, 2000);
+    }, 3000);
 });
 
-await myPromise
+const resolved = await myPromise
     .then((result) => result + 'passing by then')
     .then((result) => result + 'and now it is over!!')
     .catch((err) => console.log(err.message));
 
-
-    // After 2 seconds, it will return the value:
-    // "Resolved passing by then and now it is over!!"
-
-// -> Async/await
+    return resolved
+}
 
 
