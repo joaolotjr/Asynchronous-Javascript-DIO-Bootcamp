2051de8df970d6f30996a77af3e900b44fe859f3
joaolotjr <joaolotjr@gmail.com>
2022-06-29 15:55:41 -0300

    studying fetch

--- studies.js
+++ studies.js
@@ -15,17 +15,44 @@
 
 // // -> Async/await
 
-async function resolvePromise() {
-  const myPromise = new Promise((resolve, reject) => {
-    window.setTimeout(() => {
-      resolve('Resolved')
-    }, 3000)
-  })
+// async function resolvePromise() {
+//   const myPromise = new Promise((resolve, reject) => {
+//     window.setTimeout(() => {
+//       resolve('Resolved')
+//     }, 3000)
+//   })
 
-  const resolved = await myPromise
-    .then(result => result + 'passing by then')
-    .then(result => result + 'and now it is over!!')
-    .catch(err => console.log(err.message))
+//   const resolved = await myPromise
+//     .then(result => result + 'passing by then')
+//     .then(result => result + 'and now it is over!!')
+//     .catch(err => console.log(err.message))
 
-  return resolved
- } 
+//   return resolved
+//  }
+
+//fetch (APIs)
+
+fetch(url, options)
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+// returns a promise
+
+// fetch (APIs) - Using POST, GET, PUT, DELETE, etc.
+
+fetch('https://API-adress.com/', {
+  method: 'GET',
+  cache: 'no-cache'
+})
+  .then(response => response.json())
+  .then(json => console.log(json))
+  // returns a promise
+
+fetch('https://API-adress.com/', {
+  method: 'POST',
+  cache: 'no-cache'
+  body: JSON.stringify(data)
+})
+  .then(response => response.json())
+  .then(json => console.log(json))
+  // returns a promise  
